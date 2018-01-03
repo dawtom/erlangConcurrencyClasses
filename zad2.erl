8d6bb00ed48a786212ae5cfe6491492eea85a934
Dawid <datom2912@interia.pl>
2018-01-03 23:56:22 +0100

    Add producer and consumer

--- zad2.erl
+++ zad2.erl
@@ -10,22 +10,34 @@
 -author("Dawid Tomasiewicz").
 
 %% API
--export([start/0, server/2]).
+-export([start/0, server/2, producer/1, consumer/1, count/1]).
 
+
+count(N) ->
+  receive
+    X ->
+      if
+        N >= 10000 -> io:format("YEAAAAAH");
+        true -> count(N + X)
+      end
+  end.
+
+%%
 server(Buffer, Capacity) ->
+%%  io:format("Buffer after: ~p~n", [Buffer]),
   receive
 %%    PRODUCER
 %%  FIXME no need for number, this is redundancy, may cause problems
-    {Pid, produce, Number, InputList} ->
-
-      case canProduce(Buffer, Number, Capacity) of
+    {Pid, produce, InputList} ->
+      NumberProduce = lists:flatlength(InputList),
+      case canProduce(Buffer, NumberProduce, Capacity) of
         true ->
-%%          io:format("Producing ~p ~p~n", [Number, InputList]),
-          NewBuffer = lists:append(InputList, Buffer),
+%%          io:format("Producing ~p ~p~n", [NumberProduce, InputList]),
+          NewBuffer = append(InputList, Buffer),
           Pid ! ok,
           server(NewBuffer,Capacity);
         false ->
-%%          io:format("Cannot produce ~p ~p~n", [Number, InputList]),
+%%          io:format("Cannot produce ~p ~p~n", [NumberProduce, InputList]),
           Pid ! tryagain,
           server(Buffer, Capacity)
       end;
@@ -55,39 +67,60 @@ canProduce(Buffer, Number, Capacity) ->
 canConsume(Buffer, Number) ->
   lists:flatlength(Buffer) >= Number.
 
+producer(ServerPid) ->
+  X = rand:uniform(9),
+  ToProduce = [rand:uniform(500) || _ <- lists:seq(1, X)],
+  ServerPid ! {self(),produce,ToProduce},
+  receive
+%%    TODO handle tryagain message
+    Message -> io:format("Produce ~p ~p~n", [X,Message])
+  end,
+  producer(ServerPid).
+
+consumer(ServerPid) ->
+  X = rand:uniform(9),
+  ServerPid ! {self(),consume,X},
+  receive
+    Message -> io:format("Consume ~p ~p~n", [X,Message])
+  end,
+  consumer(ServerPid).
+
 start() ->
-  ServerPid = spawn(zad2,server,[[],10]),
+  ServerPid = spawn(zad2,server,[[],20]),
 
-  ServerPid ! {self(),produce,5,[1,4,9,16,25]},
-  receive
-    Something1 -> io:format("Produce 5: ~p~n", [Something1])
-  end,
-
-
-  ServerPid ! {self(),produce,4,[1,4,9,16]},
-  receive
-    Something2 -> io:format("Produce 4: ~p~n", [Something2])
-  end,
-
-  ServerPid ! {self(),produce,4,[1,4,9,16]},
-  receive
-    Something3 -> io:format("Produce 4: ~p~n", [Something3])
-  end,
-
-  ServerPid ! {self(),consume,3},
-  receive
-    Something4 -> io:format("Consume 3: ~p~n", [Something4])
-  end,
-
-  ServerPid ! {self(),consume,7},
-  receive
-    Something5 -> io:format("Consume 7: ~p~n", [Something5])
-  end,
-
-  ServerPid ! {self(),consume,6},
-  receive
-    Something6 -> io:format("Consume 6: ~p~n", [Something6])
-  end.
+  spawn(zad2, count, [0]),
+  spawn(zad2, producer, [ServerPid]),
+  spawn(zad2, consumer, [ServerPid]).
+%%  ServerPid ! {self(),produce,5,[1,4,9,16,25]},
+%%  receive
+%%    Something1 -> io:format("Produce 5: ~p~n", [Something1])
+%%  end,
+%%
+%%
+%%  ServerPid ! {self(),produce,4,[1,4,9,16]},
+%%  receive
+%%    Something2 -> io:format("Produce 4: ~p~n", [Something2])
+%%  end,
+%%
+%%  ServerPid ! {self(),produce,4,[1,4,9,16]},
+%%  receive
+%%    Something3 -> io:format("Produce 4: ~p~n", [Something3])
+%%  end,
+%%
+%%  ServerPid ! {self(),consume,3},
+%%  receive
+%%    Something4 -> io:format("Consume 3: ~p~n", [Something4])
+%%  end,
+%%
+%%  ServerPid ! {self(),consume,7},
+%%  receive
+%%    Something5 -> io:format("Consume 7: ~p~n", [Something5])
+%%  end,
+%%
+%%  ServerPid ! {self(),consume,6},
+%%  receive
+%%    Something6 -> io:format("Consume 6: ~p~n", [Something6])
+%%  end.
 
 
 
